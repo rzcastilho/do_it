ec9b3422e59234a54229e4abe1071854bd7ed782
rzcastilho <rodrigo.zampieri@gmail.com>
2023-07-27 20:48:54 -0300

    feat: :sparkles: add task to configure file persistence

---
+++ lib/mix/tasks/do_it.init.config.ex
@@ -0,0 +1,39 @@
+defmodule Mix.Tasks.DoIt.Init.Config do
+  use Mix.Task
+
+  import Mix.Generator
+
+  @shortdoc "Initializes DoIt persistent configuration"
+
+  @moduledoc """
+  Initializes DoIt persistent configuration.
+
+  ## Examples
+
+      $ mix do_it.init.config
+  """
+
+  embed_template :config, """
+  config :do_it, DoIt.Commfig,
+    dirname: System.tmp_dir(),
+    filename: "<%= @app %>.json"
+  """
+
+  @impl true
+  def run(_args) do
+    config = Mix.Project.config()
+    config_first_line = "import Config"
+    config_path = config[:config_path] || "config/config.exs"
+    app = config[:app] || :YOUR_APP_NAME
+    opts = [app: app]
+    case File.read(config_path) do
+      {:ok, contents} ->
+        new_contents = config_first_line <> "\n\n" <> config_template(opts)
+        Mix.shell().info [:green, "* updating ", :reset, config_path]
+        File.write!(config_path, String.replace(contents, config_first_line, new_contents))
+      {:error, _} ->
+        create_file(config_path, "import Config\n\n" <> config_template(opts))
+    end
+  end
+
+end
---
+++ test/mix/tasks/do_it.init.config_test.exs
@@ -0,0 +1,57 @@
+defmodule Mix.Tasks.DoIt.Init.ConfigTest do
+  use ExUnit.Case
+
+  import Mix.Tasks.DoIt.Init.Config, only: [run: 1]
+
+  @tmp_path Path.expand(System.tmp_dir(), __DIR__)
+
+  test "generate a new config file" do
+    in_tmp "new_config", fn ->
+
+      run([])
+
+      assert_file("config/config.exs", """
+      import Config
+
+      config :do_it, DoIt.Commfig,
+        dirname: System.tmp_dir(),
+        filename: "do_it.json"
+      """)
+    end
+  end
+
+  test "update an existing config file" do
+    in_tmp "existing_config", fn ->
+      File.mkdir_p! "config"
+      File.write! "config/config.exs", """
+      # Test
+      import Config
+      # Existing File
+      """
+
+      run([])
+
+      assert_file("config/config.exs", """
+      # Test
+      import Config
+
+      config :do_it, DoIt.Commfig,
+        dirname: System.tmp_dir(),
+        filename: "do_it.json"
+
+      # Existing File
+      """)
+    end
+  end
+
+  defp in_tmp(path, fun) do
+    path = Path.join(@tmp_path, path)
+    File.rm_rf!(path)
+    File.mkdir_p!(path)
+    File.cd!(path, fun)
+  end
+
+  defp assert_file(file, match) do
+    assert File.read!(file) =~ match
+  end
+end
